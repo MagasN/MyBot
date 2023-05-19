205c7cad2927da8ba8ad40eaaf2787da253af5a4
Nadezhda Magas <magas.nadezhda@gmail.com>
2023-05-19 20:34:42 +0300

    Added command planet

--- my_bot.py
+++ my_bot.py
@@ -1,8 +1,9 @@
 import logging
-import ephem
 import settings
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from datetime import datetime
+import ephem
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
 
 logging.basicConfig(filename='bot.log', level=logging.INFO, 
                     format="%(asctime)s %(levelname)s %(message)s")  # Запись логов информационного уровня в файл
@@ -22,30 +23,34 @@ def talk_to_me(update, context):
 
 def planet_stars(update, context):
     print("Вызван /planet")
-    update.message.reply_text("Введи, например, /planet Venus и я скажу в каком созвездии сегодня находится планета.")
-
-    message_user = update.message.text.split()[1].lower()
-    dt_now = datetime.now()
-    dt_format = dt_now.strftime('%Y/%m/%d')
     
-    if message_user =='venus':
-        update.message.reply_text(f'Venus: {(ephem.constellation(ephem.Venus(dt_format))[1])}')
-    elif message_user == 'mars':
-        update.message.reply_text(f'Mars: {(ephem.constellation(ephem.Mars(dt_format))[1])}')
-    elif message_user == 'mercury':
-         update.message.reply_text(f'Mercury: {(ephem.constellation(ephem.Mercury(dt_format))[1])}')
-    elif message_user == 'jupiter':
-        update.message.reply_text(f'Jupiter: {(ephem.constellation(ephem.Jupiter(dt_format))[1])}')
-    elif message_user == 'saturn':
-         update.message.reply_text(f'Saturn: {(ephem.constellation(ephem.Saturn(dt_format))[1])}')
-    elif message_user == 'uranus':
-         update.message.reply_text(f'Uranus: {(ephem.constellation(ephem.Uranus(dt_format))[1])}')
-    elif message_user == 'neptune':
-         update.message.reply_text(f'Eptune: {(ephem.constellation(ephem.Neptune(dt_format))[1])}')
-    elif message_user == 'pluto':
-         update.message.reply_text(f'Pluto: {(ephem.constellation(ephem.Pluto(dt_format))[1])}')
-    else:
-        update.message.reply_text("Укажи планету солнечной системы на английском.")
+    try:
+        message_user = update.message.text.split()[1].lower()
+        dt_now = datetime.now()
+        dt_format = dt_now.strftime('%Y/%m/%d')
+    
+        if message_user =='venus':
+            update.message.reply_text(f'Venus: {(ephem.constellation(ephem.Venus(dt_format))[1])}')
+        elif message_user == 'mars':
+            update.message.reply_text(f'Mars: {(ephem.constellation(ephem.Mars(dt_format))[1])}')
+        elif message_user == 'mercury':
+            update.message.reply_text(f'Mercury: {(ephem.constellation(ephem.Mercury(dt_format))[1])}')
+        elif message_user == 'jupiter':
+            update.message.reply_text(f'Jupiter: {(ephem.constellation(ephem.Jupiter(dt_format))[1])}')
+        elif message_user == 'saturn':
+            update.message.reply_text(f'Saturn: {(ephem.constellation(ephem.Saturn(dt_format))[1])}')
+        elif message_user == 'uranus':
+            update.message.reply_text(f'Uranus: {(ephem.constellation(ephem.Uranus(dt_format))[1])}')
+        elif message_user == 'neptune':
+            update.message.reply_text(f'Eptune: {(ephem.constellation(ephem.Neptune(dt_format))[1])}')
+        elif message_user == 'pluto':
+            update.message.reply_text(f'Pluto: {(ephem.constellation(ephem.Pluto(dt_format))[1])}')
+        else:
+            update.message.reply_text("Укажи планету солнечной системы на английском.")
+    except IndexError:
+        update.message.reply_text("Введи, например, /planet Venus и я скажу в каком созвездии сегодня находится планета.")
+
+    
 
 def main():
     mybot = Updater(settings.API_KEY)
